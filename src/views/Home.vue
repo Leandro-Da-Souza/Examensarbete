e5cbd9babf6d80dbaa77ea31cff30d568ead8f85
Frida Johansson <frida_mikaela@hotmail.com>
2021-05-25 11:29:02 +0200

    home-page added image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="home">
-    <h1>This Is Home</h1>
+    <div class="circle-home">
+      <p>Plantera mera, 
+      hjälper dig med trädgården sedan 2007.</p>
+    </div>
   </div>
 </template>
 
@@ -15,7 +18,29 @@ export default {
 </script>
 <style scoped lang="scss">
   #home{
-    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 43rem;
     padding-top: 6rem;
+    background-image: url('../assets/background-image-basil.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    .circle-home{
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 14rem;
+      text-align: center;
+      padding: 1rem;
+      width: 14rem;
+      p{
+        background: transparent;
+        font-weight: bold;
+      }
+      
+    }
   }
 </style>
