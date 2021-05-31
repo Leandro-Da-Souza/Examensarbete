ee6b04eac10d805aad28dd7d4940b543e55b6a93
Leandro Da Souza <dasouza.leandro@gmail.com>
2021-05-31 15:08:06 +0200

    change svg to font awesome icon, add eventlistener to input file

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import "firebase/database";
 import "firebase/auth";
+import "firebase/storage"
 
 const config = {
     apiKey: "AIzaSyCbmamFfzcAnuNmkxmhvxd1lPDfskadoYM",
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -4,9 +4,7 @@
       <form action="" class="form-wrapper">
         <h2>Lägg Till Referens</h2>
         <div class="file-upload">
-          <svg width="25" height="30" viewBox="0 0 19 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.1875 0.625H17.8125C18.4706 0.625 19 1.16836 19 1.84375V3.0625C19 3.73789 18.4706 4.28125 17.8125 4.28125H1.1875C0.529427 4.28125 0 3.73789 0 3.0625V1.84375C0 1.16836 0.529427 0.625 1.1875 0.625ZM12.6667 22.1562V15.25H17.006C17.8867 15.25 18.3271 14.1582 17.7036 13.5184L10.1779 5.78945C9.80677 5.40859 9.19818 5.40859 8.82708 5.78945L1.29141 13.5184C0.667969 14.1582 1.11328 15.25 1.99401 15.25H6.33333V22.1562C6.33333 22.8316 6.86276 23.375 7.52083 23.375H11.4792C12.1372 23.375 12.6667 22.8316 12.6667 22.1562Z" fill="#86A687"/>
-          </svg>
+          <i class="fas fa-upload fa-lg upload"></i>
           <button 
             class="ghost-btn" 
             :style="{padding: '3px', borderRadius: '6px'}"
@@ -36,7 +34,14 @@ export default {
   methods: {
     handleSubmit() {
       this.$el.querySelector('#imgUpload').click()
+    },
+    handleFileUpload(e) {
+      console.log(e.target.files[0])
     }
+  },
+  mounted() {
+    this.$el.querySelector('#imgUpload').addEventListener('change',e => this.handleFileUpload(e) 
+    )
   }
 }
 </script>
@@ -64,8 +69,13 @@ export default {
       align-items: center;
       width: 244px;
       height: 226px;
-      background-color: rgba(186,186,186, 0.3); 
+      background-color: rgba(186,186,186, 0.2); 
       border: 1px solid #bababa;
+      .upload {
+        color: $global-green-color;
+        background-color: rgba(186,186,186, 0);
+        width: 10%;
+      }
       button {
         margin-top: 10px;
       }
