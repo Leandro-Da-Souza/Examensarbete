59a00ec2ddd9efd4d8c54308fa065d94e7156315
Leandro Da Souza <dasouza.leandro@gmail.com>
2021-05-31 09:17:12 +0200

    new commit

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="auth-container">
-      <p>Loading...</p>
-  </div>
+    <div class="loader">
+		<span></span>
+		<span></span>
+		<span></span>
+ </div>
 </template>
 
 <script>
@@ -13,10 +15,10 @@ export default {
             try {
                 const res = await db.auth().signInWithEmailAndPassword(name, pass)
                 console.log(res)
-                this.$router.push({name: 'Form', meta: res})
+                this.$router.replace({name: 'Form', meta: res})
             } catch (e) {
                 console.log(e)
-                this.$router.push({name: 'Login', meta: e})
+                this.$router.replace({name: 'Login', meta: e})
             }
         }
     },
@@ -28,6 +30,43 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+@import '../variables';
+
+.loader {
+  margin-top: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loader > span {
+  display: inline-block;
+  background-color: $global-green-color;
+  opacity: 50%;
+  width: 8px;
+  height: 8px;
+  border-radius: 60%;
+  margin: 0 5px;
+  transform: translate3d(0, 0, 1);
+  animation: grow 0.6s infinite alternate;
+}
+.loader > span:nth-child(2) {
+  animation-delay: 0.25s;
+  background-color: $global-green-color;
+  opacity: 100%;
+}
+.loader > span:nth-child(3) {
+  animation-delay: 0.5s;
+}
+
+@keyframes grow {
+  to {
+    width: 16px;
+    height: 16px;
+    transform: translate3d(0, -16px, 0);
+  }
+}
+
 
 </style>
